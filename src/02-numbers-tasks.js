624d3fc62d9d0cb05b46bd0e991a96e8195fae7d
MarinaZai <marintos34@gmail.com>
2022-11-28 18:18:53 +0100

    feat: add toNumber solution

--- src/02-numbers-tasks.js
+++ src/02-numbers-tasks.js
@@ -240,8 +240,12 @@ function isPrime(n) {
  *   toNumber(42, 0) => 42
  *   toNumber(new Number(42), 0) => 42
  */
-function toNumber(/* value, def */) {
-  throw new Error('Not implemented');
+function toNumber(value, def) {
+  const bool = +value;
+  if (Number.isNaN(bool) || value === null) {
+    return def;
+  }
+  return value;
 }
 
 module.exports = {

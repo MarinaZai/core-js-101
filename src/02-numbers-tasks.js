6f0436f04c179cbbcb990f18a08c3c4f242a1504
MarinaZai <marintos34@gmail.com>
2022-11-28 16:34:45 +0100

    feat:feat: add solution parseNumberFromString

--- src/02-numbers-tasks.js
+++ src/02-numbers-tasks.js
@@ -150,8 +150,9 @@ function getLastDigit(value) {
  *     '37'     => 37
  * '-525.5'     => -525.5
  */
-function parseNumberFromString(/* value */) {
-  throw new Error('Not implemented');
+function parseNumberFromString(value) {
+  const num = Number(value);
+  return num;
 }
 
 /**

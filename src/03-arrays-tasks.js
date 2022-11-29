eae6f55b7ca23910532d9ab10d29a1ae9c28ec7e
MarinaZai <marintos34@gmail.com>
2022-11-29 17:13:02 +0100

    feat: add getMovingSum, getSecondItems, propagateItemsByPositionIndexsolutions

--- src/03-arrays-tasks.js
+++ src/03-arrays-tasks.js
@@ -240,8 +240,14 @@ function toArrayOfSquares(arr) {
  *   [ 0, 0, 0, 0, 0]         => [ 0, 0, 0, 0, 0]
  *   [ 1, 2, 3, 4, 5, 6, 7, 8, 9, 10 ] => [ 1, 3, 6, 10, 15, 21, 28, 36, 45, 55 ]
  */
-function getMovingSum(/* arr */) {
-  throw new Error('Not implemented');
+function getMovingSum(arr) {
+  const newArray = [];
+  // arr.reduce((firstItem, secondItem, index) => newArray[index] = firstItem + secondItem, 0);
+  arr.reduce((acc, item, index) => {
+    newArray[index] = acc + item;
+    return acc + item;
+  }, 0);
+  return newArray;
 }
 
 /**
@@ -255,8 +261,8 @@ function getMovingSum(/* arr */) {
  * [ 'a', 'b', 'c' , null ]  => [ "b", null ]
  * [ "a" ] => []
  */
-function getSecondItems(/* arr */) {
-  throw new Error('Not implemented');
+function getSecondItems(arr) {
+  return arr.filter((item, index) => (index + 1) % 2 === 0);
 }
 
 
@@ -274,8 +280,10 @@ function getSecondItems(/* arr */) {
  *  [ 'a', 'b', 'c', null ] => [ 'a', 'b','b', 'c','c','c',  null,null,null,null ]
  *  [ 1,2,3,4,5 ] => [ 1, 2,2, 3,3,3, 4,4,4,4, 5,5,5,5,5 ]
  */
-function propagateItemsByPositionIndex(/* arr */) {
-  throw new Error('Not implemented');
+function propagateItemsByPositionIndex(arr) {
+  const initialArr = [];
+  return arr
+    .reduce((ac, currentValue, ind) => ac.concat(Array(ind + 1).fill(currentValue)), initialArr);
 }
 
 
